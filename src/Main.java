2b62343e981dbd755f8f77d5523947306e48fef2
SOVJava <olegonoid@gmail.com>
2019-12-09 14:48:11 +0300

    add main sort

--- src/Main.java
+++ src/Main.java
@@ -7,5 +7,11 @@ public class Main {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+
+        Util.sort(arr);
+        System.out.println(" ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }

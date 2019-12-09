d24e01f6078a2f61d3989bc1f8eacd6bc0f5abe1
SOVJava <olegonoid@gmail.com>
2019-12-09 14:43:21 +0300

    add arr

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        int[] arr = new int[]{3,4,2,1,4,5,6};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
+
 }
